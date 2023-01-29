2c850c8302aa006e9508a3a813411219907b904c
Burak Turan <buraktt00@gmail.com>
2023-01-29 21:23:46 +0300

    [LineUp Screen][update desing]

--- app/src/main/java/com/example/valorantlineapp/ui/LineUpFragment.kt
+++ app/src/main/java/com/example/valorantlineapp/ui/LineUpFragment.kt
@@ -1,13 +1,30 @@
 package com.example.valorantlineapp.ui
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.valorantlineapp.R
+import com.example.valorantlineapp.base.BaseFragment
+import com.example.valorantlineapp.data.entity.Agent
+import com.example.valorantlineapp.databinding.FragmentLineUpBinding
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerFragment
+import com.google.android.youtube.player.YouTubePlayerView
 
 
-class LineUpFragment : Fragment() {
+class LineUpFragment(private val agent: Agent) : BaseFragment<FragmentLineUpBinding>(FragmentLineUpBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 
 }
